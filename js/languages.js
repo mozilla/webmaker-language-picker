60a49a7645b8a36f0dcc5f8097892738323e1271
Ali Al Dallal <ali@alicoding.com>
2014-03-04 21:02:22 -0500

    Rewrite a bit

--- js/languages.js
+++ js/languages.js
@@ -1,113 +1,105 @@
 define(['jquery', 'list', 'fuzzySearch'], function ($, List, Fuzzy) {
-  var checkExist = setInterval(function () {
-    var $currentLang;
-    var $languageList;
-    var $languageSearch;
-    var $emptyMessage;
-    var $clickLang;
+  return {
+    ready: function () {
 
-    if ($('#current-language').length) {
-      $currentLang = $('#current-language');
-      $languageList = $('#list-of-languages');
-      $languageSearch = $('#language-search');
-      $emptyMessage = $('#empty-message');
-      $clickLang = $('.langList');
+      var $currentLang = $('#current-language');
+      var $languageList = $('#list-of-languages');
+      var $languageSearch = $('#language-search');
+      var $emptyMessage = $('#empty-message');
+      var $clickLang = $('.langList');
 
       $clickLang.click(function () {
         langRedirector($(this).data().value);
       });
 
-      clearInterval(checkExist);
-    }
-
-    var fuzzy = new Fuzzy({
-      threshold: 1
-    });
-    var langs = new List('list-of-languages', {
-      valueNames: ['localized-name', 'english-name'],
-      plugins: [fuzzy]
-    });
-
-    function langRedirector(selectedLang) {
-      var matchesLang,
-        href = document.location.pathname,
-        lang = document.querySelector("html").lang,
-        supportedLanguages = $(".list").data("supported"),
-        // matches any of these:
-        // `en`, `en-us`, `en-US` or `ady`
-        matches = href.match(/([a-z]{2,3})([-]([a-zA-Z]{2}))?/);
-
-      if (matches) {
-        if (matches[1] && matches[2]) {
-          matchesLang = matches[1].toLowerCase() + matches[2].toUpperCase();
-        } else {
-          matchesLang = matches[1].toLowerCase();
-        }
-      }
-      // if the selected language is match to the language in the header
-      if (selectedLang === lang) {
-        return;
-        // check if we have any matches and they are exist in the array we have
-      } else if ((matches && matches[0]) && supportedLanguages.indexOf(matchesLang) !== -1) {
-        href = href.replace(matches[0], selectedLang);
-        window.location = href;
-      } else {
-        window.location = "/" + selectedLang + href;
-      }
-    };
-
-    $currentLang.on('click', function (e) {
-      if ($(window).width() < 500) {
-        return;
-      }
-      e.preventDefault();
-      var offset = $currentLang.position();
-
-      if ($languageList.is(':visible')) {
-        $languageList.fadeOut(100);
-        return;
-      }
-
-      $languageList.css({
-        bottom: (offset.top - 18) + 'px',
-        left: (offset.left + $currentLang.width() + 25) + 'px',
-      }).fadeIn(100);
-
-      $languageSearch.val('');
-      langs.search();
-
-      $(document).on('mousedown', function hideLanguageList() {
-        $languageList.fadeOut(100);
-        $(document).off('mousedown', hideLanguageList);
+      var fuzzy = new Fuzzy({
+        threshold: 1
+      });
+      var langs = new List('list-of-languages', {
+        valueNames: ['localized-name', 'english-name'],
+        plugins: [fuzzy]
       });
 
-    });
+      function langRedirector(selectedLang) {
+        var matchesLang,
+          href = document.location.pathname,
+          lang = document.querySelector("html").lang,
+          supportedLanguages = $(".list").data("supported"),
+          // matches any of these:
+          // `en`, `en-us`, `en-US` or `ady`
+          matches = href.match(/([a-z]{2,3})([-]([a-zA-Z]{2}))?/);
 
-    $languageList.on('mousedown', function (e) {
-      e.stopPropagation();
-    });
-
-    if ($languageList.length) {
-      $languageSearch.on('keyup', function (e) {
-        var $firstResult;
-        if (e.which === 13) {
-          $firstResult = $languageList.find('li > a:first-child');
-          if ($firstResult.length) {
-            $(this).val($firstResult.find('.localized-name').data('hint'));
-            window.location = $firstResult[0].href;
-            return;
+        if (matches) {
+          if (matches[1] && matches[2]) {
+            matchesLang = matches[1].toLowerCase() + matches[2].toUpperCase();
           } else {
-            return;
+            matchesLang = matches[1].toLowerCase();
           }
         }
-        langs.fuzzySearch.search(this.value);
-        if (!langs.visibleItems.length) {
-          $emptyMessage.show();
+        // if the selected language is match to the language in the header
+        if (selectedLang === lang) {
+          return;
+          // check if we have any matches and they are exist in the array we have
+        } else if ((matches && matches[0]) && supportedLanguages.indexOf(matchesLang) !== -1) {
+          href = href.replace(matches[0], selectedLang);
+          window.location = href;
         } else {
-          $emptyMessage.hide();
+          window.location = "/" + selectedLang + href;
         }
-      });
-    }
-  }, 100); // check every 100ms
+      };
 
+      $currentLang.on('click', function (e) {
+        if ($(window).width() < 500) {
+          return;
+        }
+        e.preventDefault();
+        var offset = $currentLang.position();
+
+        if ($languageList.is(':visible')) {
+          $languageList.fadeOut(100);
+          return;
+        }
+
+        $languageList.css({
+          bottom: (offset.top - 18) + 'px',
+          left: (offset.left + $currentLang.width() + 25) + 'px',
+        }).fadeIn(100);
+
+        $languageSearch.val('');
+        langs.search();
+
+        $(document).on('mousedown', function hideLanguageList() {
+          $languageList.fadeOut(100);
+          $(document).off('mousedown', hideLanguageList);
+        });
+
+      });
+
+      $languageList.on('mousedown', function (e) {
+        e.stopPropagation();
+      });
+
+      if ($languageList.length) {
+        $languageSearch.on('keyup', function (e) {
+          var $firstResult;
+          if (e.which === 13) {
+            $firstResult = $languageList.find('li > a:first-child');
+            if ($firstResult.length) {
+              $(this).val($firstResult.find('.localized-name').data('hint'));
+              window.location = $firstResult[0].href;
+              return;
+            } else {
+              return;
+            }
+          }
+          langs.fuzzySearch.search(this.value);
+          if (!langs.visibleItems.length) {
+            $emptyMessage.show();
+          } else {
+            $emptyMessage.hide();
+          }
+        });
+      }
+    }
+  }
 });
